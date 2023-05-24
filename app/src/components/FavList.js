9b26ed9eab114ff58c6b3ea4fa93e922e4f8d7a1
TasneemHassasneh <tasneemhassasneh890@gmail.com>
2023-05-24 23:25:53 +0300

    update and delete

--- app/src/components/FavList.js
+++ app/src/components/FavList.js
@@ -1,10 +1,31 @@
 import React, { useEffect, useState } from 'react';
 import Button from 'react-bootstrap/Button';
 import Card from 'react-bootstrap/Card';
-import { Row } from 'react-bootstrap'
+import { Row } from 'react-bootstrap';
+import axios from 'axios';
+import ModalUpdate from './ModalUpdate';
 
 const FavList = () => {
   const [movies, setMovies] = useState([]);
+  const [selectedMovie, setSelectedMovie] = useState(null);
+  const [showModal, setShowModal] = useState(false);
+
+  const handleDelete = (id) => {
+    console.log(id)
+    const serverURL = `https://movie-library-190n.onrender.com/getMovies/${id}`;
+    axios
+      .delete(serverURL)
+      .then((response) => {
+        setMovies((movies) => movies.filter((movie) => movie.id !== id));
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  };
+  const handleUpdate = (movie) => {
+    setSelectedMovie(movie);
+    setShowModal(true);
+  };
 
   useEffect(() => {
     fetch(`https://movie-library-190n.onrender.com/getMovies`)
@@ -16,20 +37,23 @@ const FavList = () => {
   return (
     <div>
       <h1>Favorite Movies</h1>
-      
+      <Row>
         {movies.map(movie => (
-            <Row>
-                <Card style={{ width: '18rem' }} key={movie.id}>
+           <Card style={{ width: '18rem' }} key={movie.id}>
              <Card.Img variant="top" src={`https://image.tmdb.org/t/p/w500${movie.poster_path}`} />
              <Card.Body>
                <Card.Title>{movie.title}</Card.Title>
-                 <Button >Delete</Button>
-                 <Button >Update</Button>
+               <Card.Text>{movie.comments}</Card.Text>
+               <Button variant="danger" onClick={() => handleDelete(movie.id)}>Delete</Button>
+                <Button variant="primary" onClick={() => handleUpdate(movie)}> Update </Button>
              </Card.Body>
            </Card>
-            </Row>
+            
              
-        ))}
+        ))}</Row>
+          {selectedMovie && (
+        <ModalUpdate movie={selectedMovie} showModal={showModal} setShowModal={setShowModal} setMovies={setMovies} />
+      )}
     </div>
   );
 };
--- app/src/components/Navbar.js
+++ app/src/components/Navbar.js
@@ -1,19 +1,23 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
+import Container from 'react-bootstrap/Container';
+import Nav from 'react-bootstrap/Nav';
+import Navbar from 'react-bootstrap/Navbar';
 
-const Navbar = () => {
+function navbar() {
   return (
-    <nav>
-      <ul>
-        <li>
-          <Link to="/">Home</Link>
-        </li>
-        <li>
-          <Link to="/favorites">Favorites</Link>
-        </li>
-      </ul>
-    </nav>
+    <Navbar bg="light" expand="lg">
+    <Container>
+      <Navbar.Brand href="#home">Movies</Navbar.Brand>
+      <Navbar.Toggle aria-controls="basic-navbar-nav" />
+      <Navbar.Collapse id="basic-navbar-nav">
+        <Nav className="me-auto">
+          <Nav.Link href="/">Home</Nav.Link>
+          <Nav.Link href="/favorites">Favorite Movie</Nav.Link>
+        </Nav>
+      </Navbar.Collapse>
+    </Container>
+  </Navbar>
   );
 };
 
-export default Navbar;
+export default navbar;
